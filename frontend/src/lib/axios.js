1a2a782d17f17f0617146daeca37b3df7e8b2871
piyush5093 <pp3620920@gmail.com>
2026-05-03 15:10:17 +0530

    axios changed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
+  baseURL: import.meta.env.VITE_API_URL || "https://talent-iq-backend-1jv1.onrender.com/api",
+  withCredentials: true,
 });
 
 export default axiosInstance;
