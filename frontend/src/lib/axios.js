0a3a18c50e59edda16330a8ef1909870f0851e24
piyush5093 <pp3620920@gmail.com>
2026-05-03 15:15:26 +0530

    agian changed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://talent-iq-backend-1jv1.onrender.com/api",
+  baseURL: "https://talent-iq-backend-1jv1.onrender.com/api",
   withCredentials: true,
 });
 
